75bf3a597cd02733100ce762073c529ec4efcf52
Kanha Agarwal <kanhaparul08@gmail.com>
2026-06-27 04:27:03 +0530

    Commit

---
+++ Strings/indexoffirstoccurenceinstring.py
@@ -0,0 +1,25 @@
+haystack = input("Enter haystack string: ")
+needle = input("Enter needle string: ")
+haystack = haystack.lower()
+needle = needle.lower()
+length = 0
+indexl = []
+i = 0
+while i!=len(haystack)-1:
+    if haystack[i]==needle[length]:
+        length+=1
+        indexl.append(i)
+        #print(haystack[i])
+        if len(needle)==length:
+            print(indexl[0])
+            break
+        i+=1
+    else:
+        if len(indexl)!=0:
+            i=indexl[0]
+        else:
+            i+=1
+        length = 0
+        indexl = []
+if length!=len(needle):
+    print(-1)
