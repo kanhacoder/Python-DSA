14902c15bd4d94ac29b601dccc562be5f481adac
Kanha Agarwal <kanhaparul08@gmail.com>
2026-06-27 05:31:48 +0530

    Commit

---
+++ Strings/validpalindrome.py
@@ -0,0 +1,10 @@
+string1 = "A man, a plan, a canal: Panama"
+nstr = ""
+for i in string1:
+    if i.isalpha():
+        nstr+=i.lower()
+rstr = nstr[::-1]
+if rstr==nstr:
+    print("True")
+else:
+    print("False")
